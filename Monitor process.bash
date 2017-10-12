d0d359f15507c7bd541d3d2b979f187f03e22a65
Jay.Cheng <815325223@qq.com>
2017-10-12 11:04:41 +0800

    Monitor the Serv-U
    Monitor the Serv-U.

---
+++ Monitor process.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Usage: */5 * * * * /bin/bash /root/monitor.sh >> /root/monitor.log 2>&1
+process_name="/usr/local/Serv-U/Serv-U"
+process_count=1
+
+process_number()
+{
+        number=`ps -ef | grep ${process_name} | grep -v grep | wc -l`
+        return ${number}
+}
+
+process_number
+
+p_num=$?
+
+now=`date '+%Y-%m-%d %H:%M:%S'`
+
+if [ ${p_num} -lt ${process_count} ]
+then
+        "/usr/local/Serv-U/Serv-U" -startservice
+        echo "[$now] [ERROR] '$process_name' process_number is $p_num."
+        echo "Please check the service!." | mail -s "SFTP notification" jay@example.com
+else
+        echo "[$now] [DEBUG] '$process_name' process_number is $p_num, status ok."
