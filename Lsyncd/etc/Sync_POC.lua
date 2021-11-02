c00f1864d6e2ad39fed5916aa2a35fbe819025a3
Jay.Cheng <31837260+815325223@users.noreply.github.com>
2021-11-02 09:57:19 +0800

    Update Sync_POC.lua

--- Lsyncd/etc/Sync_POC.lua
+++ Lsyncd/etc/Sync_POC.lua
@@ -1,1 +1,17 @@
+settings {
+    logfile = "/var/log/Sync_POC/Sync_POC.log",
+    statusFile = "/var/log/Sync_POC/Sync_POC-status.log",
+    statusInterval = 10
+}
 
+sync {
+    default.rsync,
+    source = "/POC",
+    target = "client.devops.com:/POC",
+    init = false,
+    rsync = {
+        compress = true,
+        acls = true,
+        verbose = true,
+        rsh = "/usr/bin/ssh -p 22 -o StrictHostKeyChecking=no -i /home/SyncUser/.ssh/id_rsa -l SyncUser" }
+}
