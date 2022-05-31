f55c94a25dcf6c143bd86a566374a356f64d73b1
Jay.Cheng <31837260+815325223@users.noreply.github.com>
2022-05-31 16:10:19 +0800

    Update GitLabUtils.py

--- GitLab/GitLabUtils.py
+++ GitLab/GitLabUtils.py
@@ -1,22 +1,22 @@
+import sys
 import requests
 import io
-import sys
 import json
 
 class GitLabUtils():
 
-  def __init__(self, projectID):
-    self.gitlab_url = "https://gitlab.brooks.com/api/v4"
+  def __init__(self, ProjectID):
+    self.gitlab_url = "http://gitlab.devops.local/api/v4"
     self.gitlab_token = "<GITLAB_TOKEN>"
-    self.projectID = projectID
-  
+    self.ProjectID = ProjectID
+
   def http_req(self, method, apiURL, headers, data):
     url = "{0}/{1}".format(self.gitlab_url, apiURL)
     response = requests.request(method, url, headers=headers, data=data)
     return response
 
   def get_repo_file(self, filePath, branch, targetFile):
-    apiurl = "/projects/{0}/repository/files/{1}/raw?ref={2}".format(self.projectID, filePath, branch)
+    apiurl = "/projects/{0}/repository/files/{1}/raw?ref={2}".format(self.ProjectID, filePath, branch)
     headers = {
       'PRIVATE-TOKEN': self.gitlab_token,
       'Content-Type': 'application/json'
@@ -28,9 +28,9 @@ class GitLabUtils():
     with io.open(filePath, 'w', encoding='utf-8') as f:
       f.write(content)
       f.close()
-
+  
   def create_repo_file(self, filePath, branch, content, commit_message):
-    apiurl = "/projects/{0}/repository/files/manifests%2F{1}".format(self.projectID, filePath)
+    apiurl = "/projects/{0}/repository/files/manifests%2f{1}".format(ProjectID, filePath)
     data = json.dumps(
       {
         "branch": branch,
@@ -43,11 +43,11 @@ class GitLabUtils():
       'Content-Type': 'application/json'
     }
     msg = self.http_req("POST", apiurl, headers, data)
-    if msg.json()["message"] == "A file with this name already exists":
+    if msg.status_code == 400:
       raise Exception("A file with this name already exists.")
-
+  
   def update_repo_file(self, filePath, branch, content, commit_message):
-    apiurl = "/projects/{0}/repository/files/manifests%2F{1}".format(self.projectID, filePath)
+    apiurl = "/projects/{0}/repository/files/manifests%2f{1}".format(ProjectID, filePath)
     data = json.dumps(
       {
         "branch": branch,
@@ -61,20 +61,18 @@ class GitLabUtils():
     }
     self.http_req("PUT", apiurl, headers, data)
 
-if __name__ == '__main__':
-  # python GitLabUtils.py getfile 19 default.yaml master template.yaml
+if __name__ == "__main__":
   if sys.argv[1] == "getfile":
-    projectID, filename, branch, targetFile = sys.argv[2:]
-    GitLabUtils(projectID).get_repo_file(filename, branch, targetFile)
-  
-  # python GitLabUtils.py updatefile 19 template.yaml master release-2.0.3.yaml
+    ProjectID, filename, branch, targetFile = sys.argv[2:]
+    GitLabUtils(ProjectID).get_repo_file(filename, branch, targetFile)
+
   if sys.argv[1] == "updatefile":
-    projectID, filename, branch, targetFile = sys.argv[2:]
+    ProjectID, filename, branch, targetFile = sys.argv[2:]
     with io.open(filename, 'r', encoding='utf-8') as f:
       content = f.read()
       f.close()
     try:
-      GitLabUtils(projectID).create_repo_file(targetFile, branch, content, "created by ZCHENG.")
+      GitLabUtils(ProjectID).create_repo_file(targetFile, branch, content, "created by ZCHENG.")
     except Exception as e:
       print(e)
-      GitLabUtils(projectID).update_repo_file(targetFile, branch, content, "updated by ZCHENG.")
+      GitLabUtils(ProjectID).update_repo_file(targetFile, branch, content, "updated by ZCHENG.")
