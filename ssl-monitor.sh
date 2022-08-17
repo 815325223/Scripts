06dd7c543fbfbb355056d63aca5d01aa2ecf2911
Jay.Cheng <31837260+815325223@users.noreply.github.com>
2022-08-17 10:46:54 +0800

    Create ssl-monitor.sh

---
+++ ssl-monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WebName=$1
+Port="443"
+
+Cert_END_Time=$(echo | openssl s_client -servername ${WebName} -connect ${WebName}:${Port} 2> /dev/null | openssl x509 -noout -dates | grep 'After' | awk -F '=' '{print $2}' | awk '{print $1,$2,$4}')
+Cert_NED_TimeStamp=$(date +%s -d "$Cert_END_Time")
+Create_TimeStamp=$(date +%s)
+Rest_Time=$(expr $(expr $Cert_NED_TimeStamp - $Create_TimeStamp) / 86400)
+echo "$WebName SSL certificate has $Rest_Time days left to expire." > /home/zcheng/check_cert/ssl-monitor.txt
+if [ $Rest_Time -lt 30 ];then
+  WebHook='webhook incoming'
+  curl "${WebHook}" -H 'Content-Type: application/json' -d '
+  {
+    "text": "'"$(cat ./ssl-monitor.txt)"'"
+  }' &> /dev/null
+fi
