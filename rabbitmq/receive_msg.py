758878180dcd50f3b10c927597ab7f9b2864b58b
Jay.Cheng <31837260+815325223@users.noreply.github.com>
2022-09-28 13:50:11 +0800

    Create receive_msg.py

---
+++ rabbitmq/receive_msg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import signal
+import pika
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+credentials = pika.PlainCredentials('serverworld', 'strongpassword')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+                                     'localhost',
+                                     5672,
+                                     '/my_vhost',
+                                     credentials))
+
+channel = connection.channel()
+channel.queue_declare(queue='Hello_World')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(callback,
+                      queue='Hello_World',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
